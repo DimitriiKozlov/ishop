a50884be413891589993735a034da50b816a6d95
Dimitrii <ikzdimitrij@gmail.com>
2017-06-09 23:14:44 +0300

    init

---
+++ mobiles/urls.py
@@ -0,0 +1,23 @@
+from mobiles import views
+
+__author__ = 'oleh'
+
+from django.conf.urls import url
+
+from django.contrib.auth.views import login, logout
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+
+    url(r'^login/$', login),
+    url(r'^logout/$', logout),
+    url(r'^signup/$', views.signup, name='signup'),
+
+
+    url(r'^(?P<mobile_id>\d+)/$', views.detail, name='detail'),
+    url(r'^card/$', views.card, name='results'),
+
+    url(r'^card/remove/(?P<mobile_id>\d+)/', views.remove, name='results'),
+
+    url(r'^(?P<mobile_id>\d+)/buy/$', views.buy, name='buy'),
+]
